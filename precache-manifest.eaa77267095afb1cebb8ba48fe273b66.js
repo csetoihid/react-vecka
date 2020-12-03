0930ee4a562ed049fddf9340e72e3ee3055384e8
csetoihid <cse.toihid@gmail.com>
2020-12-03 23:52:43 +0100

    Updates

---
+++ precache-manifest.eaa77267095afb1cebb8ba48fe273b66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab0a2c83b1dbbe914b9d7919f15af5c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "437db6f57c39d4a51f90",
+    "url": "./static/css/main.b3ef8317.chunk.css"
+  },
+  {
+    "revision": "50a00034dd0822f6667a",
+    "url": "./static/js/2.d64d324b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d64d324b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "437db6f57c39d4a51f90",
+    "url": "./static/js/main.11aa7fb2.chunk.js"
+  },
+  {
+    "revision": "7e5a172db042f5cdd9e9",
+    "url": "./static/js/runtime-main.f6e4338a.js"
+  }
+]);
